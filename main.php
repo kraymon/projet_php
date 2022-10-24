d4ede394ced6d6f2f534a4a727fb7017041f21f6
Yessi <yessicerlyn@gmail.com>
2022-10-24 12:17:45 +0200

    TEST acceuil

--- main.php
+++ main.php
@@ -14,6 +14,27 @@
 </head>
 <body>
     <h1>subject ?</h1>
+<table>
+<tr>
+			<td> <h1> CREER UN EVENEMENTS </h1>
+			<p> Créer votre équipe de 5 avec vos joueurs préférer </p> </td>
+			<td>
+			<h1> REJOINDRE UN EVENEMENTS </h1>
+			<p> inscriver un joueur </p>
+			</td>
+</tr>
+
+<tr>
+			<td> <h1> INSCRIRE UN JOUEUR </h1>
+			<p> Créer votre équipe de 11 avec vos joueurs préférer </p> </td>
+			<td>
+			<h1> CREER UNE EQUIPE</h1>
+			<p> sélectionner votre 5</p>
+			</td>
+</tr>
+
+</table>
+
 
 </body>
 </html>
