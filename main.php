c6fe5a5e17a015ef9eef2dceae03004a897dd89c
Abdel Malik Djendli <a.malik.djendli@gmail.com>
2022-10-24 10:37:54 +0200

    Choix sujet

---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<head>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="">
+	<link rel="icon" href="">
+	<script src=""></script>
+	<script src="https://kit.fontawesome.com/d82b06789a.js" crossorigin="anonymous"></script>
+	
+	
+	<title>Acceuil</title>
+	
+</head>
+<body>
+    <h1>Sujet ?</h1>
+
+</body>
+</html>
